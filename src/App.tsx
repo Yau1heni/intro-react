d226708b82461b059e00f4ebef37a8ee64bf02ed
Yau1heni <lodbrok9@gmail.com>
2022-09-10 18:26:02 +0300

    02 Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,62 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log('App rendering')
     return (
         <div>
-            This is App Component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
 
     );
 }
 
+function AppTitle() {
+    return <>This is App Component</>
+}
+
 function Rating() {
+    console.log('Rating rendering')
     return (
-        <div>
+        <>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
-        </div>)
+        </>)
 }
 
 function Star() {
+    console.log('Star rendering')
     return (
         <div>star</div>
     )
 }
 
 function Accordion() {
+    console.log('Accordion rendering')
     return (
-        <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
+        <>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </>
+    )
+}
+
+function AccordionTitle() {
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     )
 }
 
